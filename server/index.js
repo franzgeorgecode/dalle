f6ad5698ce17eca84f67ad613277fbada0942f2f
Franz George Muller Rueda <118325804+franzgeorgecode@users.noreply.github.com>
2023-02-08 13:18:49 -0500

    actualizando puerto

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
+const puerto= process.env.PORT
+
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
@@ -24,7 +26,7 @@ app.get('/', async (req, res) => {
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server started on port http://localhost:8080'));
+    app.listen(puerto, () => console.log('Server started on port'));
   } catch (error) {
     console.log(error);
   }
